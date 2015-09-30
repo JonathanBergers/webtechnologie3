8f14c2eef54dbfdde953a5d86e7c7da176d3c97d
jonathan <jonathan.bergers@gmail.com>
2015-09-30 11:26:19 +0200

    Model

---
+++ src/model/Model.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by jonathan on 30-9-15.
+ */
+public class Model {
+}
---
+++ src/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * Created by jonathan on 30-9-15.
+ *
+ *o Achternaam
+ o Tussenvoegsels
+ o Voornaam
+ o Nickname
+ o Wachtwoord
+ *
+ */
+public class User {
+
+
+
+
+
+}

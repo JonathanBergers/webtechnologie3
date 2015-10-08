c03e6178eef7bef1bda6c3fbedcd4a04e8d185d4
Falco <Falcotjeijben@gmail.com>
2015-10-08 13:52:39 +0200

    Rating eerste v1.0

---
+++ src/model/Rating.java
@@ -0,0 +1,45 @@
+package model;
+
+/**
+ * Created by falco on 6-10-15.
+ */
+public class Rating {
+
+    private int stars;
+    private User user;
+    private Movie movie;
+
+
+    public Rating(int stars, User user, Movie movie) {
+        setStars(stars);
+
+        this.user = user;
+        this.movie = movie;
+    }
+
+    public Rating(){
+
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        if(stars<1){
+            this.stars = 1;
+        } else if(stars>10){
+            this.stars = 10;
+        } else {
+            this.stars = stars;
+        }
+
+    }
+    public User getUser(){
+        return this.user;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+}
